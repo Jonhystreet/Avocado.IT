30a42fe1c6f1b7d5238bb69c858de34701935c77
Jonhystreet <64434461+Jonhystreet@users.noreply.github.com>
2021-02-04 14:29:59 -0600

    Delete AvocadoIT directory

--- AvocadoIT/src/AvocadoIT.jl
+++
@@ -1,9 +0,0 @@
-#
-
-module AvocadoIT
-    using Pkg,Printf, CSV, JuliaDB, Distributions, OnlineStats, DataFrames, Dates, Plots, PyCall, HTTP, ZipFile, Logging, TerminalLoggers, Dates, XLSX, ExcelReaders
-    using CSVFiles
-    include("DescargaDatos.jl")
-    include("Repositorios.jl")
-    include("CombinaDatos.jl")
-end
